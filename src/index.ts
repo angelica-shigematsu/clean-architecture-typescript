fbf1dc63e209466ac8adec4678f6e75e99a83945
Angélica <angelica_shigematsu@outlook.com>
2023-12-11 18:46:33 -0300

    feat: added logincontroller in index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import express from 'express'
 import UserCaseUse from './usecase/UserCaseUse';
 import UserRepositoryBD from './infra/db/UserRepositoryBD';
 import CreateUserController from './infra/api/CreateUserController';
+import LoginUserCaseUse from './usecase/LoginUserCaseUse';
+import TransofrmPasswordCrypt from './infra/auth/TransformPasswordCrypt';
+import LoginUserController from './infra/api/LoginUserController';
 
 dotenv.config()
 
@@ -13,9 +16,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 const repositoryUser = new UserRepositoryBD()
-const userCase = new UserCaseUse(repositoryUser)
+const passwordCrypt = new TransofrmPasswordCrypt()
+const userCase = new UserCaseUse(repositoryUser, passwordCrypt)
+
+const loginCase = new LoginUserCaseUse(repositoryUser, passwordCrypt)
 
 new CreateUserController(app, userCase)
+new LoginUserController(app, loginCase)
+
 
 app.listen(process.env.API_PORT, () => {
   console.log('listening on port ' + process.env.API_PORT)
